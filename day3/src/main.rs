bf635cf37b99984f6afc1de2af5735dcf512c548
Joe Ardent <code@ardent.nebcorp.com>
2019-12-14 13:01:39 -0800

    Augments Point with steps; same behavior as part 1.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,11 +1,13 @@
-use std::cmp::{max, min, Eq};
-use std::collections::HashSet;
+use std::cmp::{max, min, Eq, PartialEq};
+use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
+use std::hash::{Hash, Hasher};
 
-#[derive(Copy, Clone, Hash, Eq, PartialEq, Debug)]
+#[derive(Copy, Clone, Debug)]
 struct Point {
     pub x: i32,
     pub y: i32,
+    pub steps: u32,
 }
 
 impl Point {
@@ -13,6 +15,21 @@ impl Point {
         (self.x.abs() + self.y.abs()) as u32
     }
 }
+
+impl Hash for Point {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+    }
+}
+
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl Eq for Point {}
 
 fn get_input() -> Vec<Vec<String>> {
     let wires: Vec<_> = include_str!("input").trim().split('\n').collect();
@@ -30,6 +47,7 @@ fn parse_input(input: &[Vec<String>]) -> Vec<HashSet<Point>> {
     for wire in input {
         let mut prev_x: i32 = 0;
         let mut prev_y: i32 = 0;
+        let mut steps: u32 = 0;
 
         let mut points = HashSet::<Point>::new();
         for d in wire.iter() {
@@ -41,8 +59,12 @@ fn parse_input(input: &[Vec<String>]) -> Vec<HashSet<Point>> {
                 "D" => {
                     new_y = prev_y - n;
                     for y in new_y..prev_y {
-                        let p = Point { x: prev_x, y };
-                        // dbg!(&p); // left in to leave a human trace of my process; each case had a dbg!.
+                        steps += 1;
+                        let p = Point {
+                            x: prev_x,
+                            y,
+                            steps,
+                        };
                         points.insert(p);
                     }
                     prev_y = new_y;
@@ -50,7 +72,12 @@ fn parse_input(input: &[Vec<String>]) -> Vec<HashSet<Point>> {
                 "U" => {
                     new_y = prev_y + n;
                     for y in prev_y..=new_y {
-                        let p = Point { x: prev_x, y };
+                        steps += 1;
+                        let p = Point {
+                            x: prev_x,
+                            y,
+                            steps,
+                        };
                         points.insert(p);
                     }
                     prev_y = new_y;
@@ -58,7 +85,12 @@ fn parse_input(input: &[Vec<String>]) -> Vec<HashSet<Point>> {
                 "L" => {
                     new_x = prev_x - n;
                     for x in new_x..prev_x {
-                        let p = Point { x, y: prev_y };
+                        steps += 1;
+                        let p = Point {
+                            x,
+                            y: prev_y,
+                            steps,
+                        };
                         points.insert(p);
                     }
                     prev_x = new_x;
@@ -66,7 +98,12 @@ fn parse_input(input: &[Vec<String>]) -> Vec<HashSet<Point>> {
                 "R" => {
                     new_x = prev_x + n;
                     for x in prev_x..=new_x {
-                        let p = Point { x, y: prev_y };
+                        steps += 1;
+                        let p = Point {
+                            x,
+                            y: prev_y,
+                            steps,
+                        };
                         points.insert(p);
                     }
                     prev_x = new_x;
@@ -88,7 +125,13 @@ fn main() {
     assert!(&input.len() == &(2 as usize));
 
     let mut intersection: HashSet<_> = input[0].intersection(&input[1]).collect();
-    let _ = intersection.remove(&(Point { x: 0, y: 0 }));
+    let _ = intersection.remove(
+        &(Point {
+            x: 0,
+            y: 0,
+            steps: std::u32::MAX, // steps are not considered part of the identity, so value is irrelevant
+        }),
+    );
 
     for p in intersection.iter() {
         dbg!(p);
