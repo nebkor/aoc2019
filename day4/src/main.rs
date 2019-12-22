e507fade4c9944c103575ec5eef66dc226f45b28
Joe Ardent <code@ardent.nebcorp.com>
2019-12-22 13:51:17 -0800

    Day 4, part 2 in the bag.

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 const LOW: u32 = 156218;
 const HIGH: u32 = 652527 + 1;
 
@@ -6,8 +8,14 @@ fn main() {
     for guess in LOW..HIGH {
         let s = guess.to_string();
         let digits: Vec<u32> = s.chars().map(|d| d.to_digit(10).unwrap()).collect();
-        if digits.windows(2).all(|w| !(w[1] < w[0])) && digits.windows(2).any(|w| w[0] == w[1]) {
-            num_pwords += 1;
+        if digits.windows(2).all(|w| !(w[1] < w[0])) {
+            let mut counts: HashMap<u32, u32> = HashMap::new();
+            for d in digits.iter() {
+                *counts.entry(*d).or_insert(0) += 1;
+            }
+            if counts.values().any(|v| *v == 2) {
+                num_pwords += 1;
+            }
         }
     }
     println!("num passwords: {}", num_pwords);
